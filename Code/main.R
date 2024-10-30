129220f6ee02468e0e665dadf2a737af6b75121b
Chenkai_Zhao <ck_zhao@163.com>
2024-10-30 16:11:02 +0800

    Update

--- Code/main.R
+++ Code/main.R
@@ -2,7 +2,7 @@ library(httpgd)
 hgd()
 hgd_view()
 
-#------------------------------------------
+# ---------------------------------------------------------------------------
 library(Rcpp)
 library(deSolve)
 library(readxl)
@@ -15,121 +15,99 @@ library(truncnorm)
 library(extraDistr)
 library(coda)
 
+# save.image(file = paste0(FilePath, "20241015.Rdata"))
+load("Output/V1/20241015.Rdata")
+
 source("Code/func.R")
 source("Code/RefDat.R")
 # source("Code/RefDat_MA.R")
 
 sourceCpp("Code/model_cppV4.cpp")
-# sourceCpp("Code/model_SEIR_cpp.cpp")
+sourceCpp("Code/model_immuV4.cpp")
 
 Scot_Pop <- read_excel("Pop.xlsx", sheet = "Scot_Pop")
 Scot_Pop <- Scot_Pop[5, -1]
 ContacrStr <- fread("ContRatePerCapita.csv")
 ContacrStr <- ContacrStr[, -1] %>% as.matrix()
-Scot_HospRate <- c(59.6206, 44.81968, 24.28517, 9.1871, 2.72915, 0.42175, 0.19087, 0.92575, 0.92575, 0.92575, 4.340780728)
+Scot_HospRate <- c(
+  59.6206, 44.81968, 24.28517, 9.1871, 2.72915,
+  0.42175, 0.19087, 0.92575, 0.92575, 0.92575, 4.340780728
+)
 
-#------------------------------------------
+# ---------------------------------------------------------------------------
 FilePath <- File.CreateMainFolder(path = "Output/", FolderName = "V1")
 
-# Model calibration, load parameters and plot calibration results
-source("Code/Calibration.R")
+# Model calibration ---------------------------------------------------------
+### Model calibration, load parameters and plot calibration results
+source("Code/Calibration/Calibration.r")
 
-# Find the best parameter set (by serological prevalence)
-source("Code/Serological.r")
+# Immunisation ---------------------------------------------------------------
+### Run immunisation scenarios
+source("Code/Immunisation/Immune.R")
 
+# Section 1 ------------------------------------------------------------------
+### Find the best parameter set (by serological prevalence)
+source("Code/1.Serological.r")
+save(Serologocal_CI, file = paste0(FilePath, "Serologocal_CI.Rdata"))
 
+# Plot age-specific incidence rate
+source("Code/2.IncidenceRate.r")
 
+a1 <- Model.RunSim(Parm = Parameter.Create(
+  beta_base = Posteriori_Median_Sbase[1],
+  beta_seasonal = Posteriori_Median_Sbase[2],
+  phi = Posteriori_Median_Sbase[3],
+  seasonal_wavelength = Posteriori_Median_Sbase[4],
+  Hosp_rate = rep(1, 11),
+  Age_Sus = c(1, 1)
+), lag = FALSE)[[2]]
 
-#############      Chain 1
-set.seed(971889)
-Chain1New <- MCMC.MH(Prior = c(
-  1, # beta_base
-  1.5, # beta_seasonal
-  0.52, # phi
-  0.09, # seasonal_wavelength
-  0.029, # 0-2 months
-  0.029, # 3-5 months
-  0.010, # 6-11 months
-
-  0.010, # 1-2 years
-  0.015, # 2-4 years
-
-  0.015, # 5-19 years
-  0.008, # 20-59 years
-
-  0.08, # 60-64 years
-  0.08, # 65-69 years
-  0.08, # 70-74 years
-
-  0.08 # 75+ years
-), n_iterations = 30, TargetDat = RefDat, lag = FALSE, Sus_reduce = 0.02)
-# save(Chain1, file = "Chain1.Rdata")
-load("Chain1.Rdata")
-
-Chain1 <- S_base[[2]]
-Chain1 <- S_42[[2]]
-Chain1 <- S_44[[3]]
-Chain1 <- S_62[[3]]
-Chain1 <- S_63[[2]]
-Chain1 <- scenario_3.2[[1]]
-
-Chain1 <- Chain1New #
-MCMC.TracePlot(Chain1)
-
-#------------------------------------------
-Chain1Sim <- Model.RunSim(
-  Parm = Parameter.Create(
-    beta_base = Chain1[dim(Chain1)[1], 1],
-    beta_seasonal = Chain1[dim(Chain1)[1], 2],
-    phi = Chain1[dim(Chain1)[1], 3],
-    seasonal_wavelength = Chain1[dim(Chain1)[1], 4],
-    Hosp_rate = c(
-      Chain1[dim(Chain1)[1], 5], Chain1[dim(Chain1)[1], 6], Chain1[dim(Chain1)[1], 7], Chain1[dim(Chain1)[1], 8],
-      Chain1[dim(Chain1)[1], 9], Chain1[dim(Chain1)[1], 10], Chain1[dim(Chain1)[1], 11], Chain1[dim(Chain1)[1], 12],
-      Chain1[dim(Chain1)[1], 13], Chain1[dim(Chain1)[1], 14], Chain1[dim(Chain1)[1], 15]
-    ),
-    Age_Sus = c(0.4, 0.2)
-  ),
-  lag = FALSE
-)
-
-Parm$Age_Sus <- c(1, 1, 1, 1, 1, 1, 0.8, 0.8, 0.8, 0.8, 0.8)
-Parm$phi <- 0
-Parm$gamma <- c(rep(1 / 2, 4), rep(1 / 7, 7)) #
-Chain1Sim <- Model.RunSim(
-  Parm = Parm,
-  lag = FALSE
-)
-
-Model.RunSim.LLH(
-  Parm = Parameter.Create(
-    beta_base = Chain1[dim(Chain1)[1], 1],
-    beta_seasonal = Chain1[dim(Chain1)[1], 2],
-    phi = Chain1[dim(Chain1)[1], 3],
-    seasonal_wavelength = Chain1[dim(Chain1)[1], 4],
-    Hosp_rate = c(
-      Chain1[dim(Chain1)[1], 5], Chain1[dim(Chain1)[1], 6], Chain1[dim(Chain1)[1], 7], Chain1[dim(Chain1)[1], 8],
-      Chain1[dim(Chain1)[1], 9], Chain1[dim(Chain1)[1], 10], Chain1[dim(Chain1)[1], 11], Chain1[dim(Chain1)[1], 12],
-      Chain1[dim(Chain1)[1], 13], Chain1[dim(Chain1)[1], 14], Chain1[dim(Chain1)[1], 15]
-    ),
-    Age_Sus = c(0.6, 0.2), # 0.02,
-  ),
-  lag = FALSE, AgeDistribution = TRUE
-)
+a1 <- a1[time >= as.Date("2019-06-20")]
+a1[, (3:13) := lapply(.SD, sum), .SDcols = 3:13]
+a1_IR <- as.numeric(a1[1, 3:13]) / Scot_Pop
 
 
-Chain1Sim <- Model.RunSim(Parm = Parameter.Create(
-  beta_base = 0.88, # Chain1[dim(Chain1)[1], 1], # 0.7, # 2.436346
-  beta_seasonal = 11, # Chain1[dim(Chain1)[1], 2], # 12, # 1.615794
-  phi = -10, # Chain1[dim(Chain1)[1], 3], # -30, # 0.5156857
-  seasonal_wavelength = Chain1[dim(Chain1)[1], 4], # 25, # 0.11161094
-  Hosp_rate = # c(.1, .1, .01, .01, .01, .001, .001, .001, .01, .01, .001),
-    c(
-      Chain1[dim(Chain1)[1], 5], Chain1[dim(Chain1)[1], 6], Chain1[dim(Chain1)[1], 7], Chain1[dim(Chain1)[1], 8],
-      Chain1[dim(Chain1)[1], 9], Chain1[dim(Chain1)[1], 10], Chain1[dim(Chain1)[1], 11], Chain1[dim(Chain1)[1], 12],
-      Chain1[dim(Chain1)[1], 13], Chain1[dim(Chain1)[1], 14], Chain1[dim(Chain1)[1], 15]
-    ),
+a2 <- Model.RunSim(Parm = Parameter.Create(
+  beta_base = Posteriori_Median_S42[1],
+  beta_seasonal = Posteriori_Median_S42[2],
+  phi = Posteriori_Median_S42[3],
+  seasonal_wavelength = Posteriori_Median_S42[4],
+  Hosp_rate = rep(1, 11),
+  # c(
+  # 0.0986271103, 0.0457296935, 0.0298217308, 0.0097070635 * 2, 0.0015671755,
+  # 0.0003507343, 0.0003733301, 0.0025458518, 0.0038241801, 0.0062840480, 0.0054163341
+  # ), # rep(1, 11),
   Age_Sus = c(0.4, 0.2)
-), lag = F)
-Plot.Model(Chain1Sim, RealDat = RealDat_plot)[[1]]
-Plot.Model(Chain1Sim, RealDat = RealDat_plot)[[3]]
+), lag = FALSE)[[2]]
+
+a2 <- a2[time >= as.Date("2019-06-20")]
+a2[, (3:13) := lapply(.SD, sum), .SDcols = 3:13]
+a2_IR <- as.numeric(a2[1, 3:13]) / Scot_Pop
+
+
+# Plot age-specific infection-hospitalisation ratio
+source("Code/3.I2H.r")
+
+# Section 2 ------------------------------------------------------------------
+#### For Figure 2 and appendix -----------------------------------------------
+### Plot proportion & net reduction in all age groups
+source("Code/4.Portect_All.r")
+
+### Plot proportion & net reduction in immunised and non-immunised groups
+source("Code/6.Portect_TwoGroup.r")
+
+### Plot Average reduction per vaccine
+source("Code/5.AvertHospPerVac.r")
+
+### Plot age-specific incidence rate after immunisation
+source("Code/7.IncidenceRate_AfterVac.r")
+
+
+#### For Figure 3 -----------------------------------------------------------
+source("Code/8.Figure3.r")
+
+### Age distribution for appendix
+source("Code/9.AgeDis.r")
+
+### Peak reduction for appendix
+source("Code/10.PeakReduction.r")
